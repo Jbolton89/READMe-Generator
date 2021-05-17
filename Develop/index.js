2817535b7680da96072325488e317e536cdbf26b
Jbolts <77908382+Jbolton89@users.noreply.github.com>
2021-05-17 18:04:06 +1000

    another attempt

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,5 @@
 // TODO: Include packages needed for this application
-const fs = require("fs"); 
+const fs = require("fs");
 const inquirer = require('inquirer');
 const fetch = require('node-fetch');
 const util = require('util');
@@ -13,68 +13,68 @@ const queries = [
         message: "What is the title of your project?"
     },
     {
-        type:"input",
+        type: "input",
         name: "description",
         message: "What would you like as a description?"
     },
     {
-        type:"input",
+        type: "input",
         name: "installation",
         message: "What are your installation instructions"
     },
     {
-        type:"input",
+        type: "input",
         name: "usage",
         message: "what is the project usage"
     },
     {
-        type:"input",
+        type: "input",
         name: "license",
         message: "Please provide a license and/or badge link"
     },
     {
-        type:"input",
+        type: "input",
         name: "contributing",
         message: "Please type the contributing parties"
     },
     {
-        type:"input",
-        name:"test",
-        message:"What are your project tests?"
+        type: "input",
+        name: "test",
+        message: "What are your project tests?"
     },
     {
-        type:"input", 
-        name:"username",
-        message:"Please enter your Github username"
+        type: "input",
+        name: "username",
+        message: "Please enter your Github username"
     },
     {
-        type:"input",
-        name:"email",
-        message:"Please enter your email address"
+        type: "input",
+        name: "email",
+        message: "Please enter your email address"
     }
 ];
 
-inquirer
-    .prompt(queries)
-    .then(function(results){ 
+inquirer.prompt(queries)
+    .then(function (results) {
         const url = `https://api.github.com/users/${results.username}`;
-        fetch(url).then(function(Response) {
+        fetch(url).then(function (result) {
             const githubInformation = {
-                email: fetch.Response.results.email, 
-                image:fetch.Response.results.avatar_url, 
+                email: fetch.Response.results.email,
+                image: fetch.Response.results.avatar_url,
                 name: fetch.Response.results.name,
-                profile: fetch.Response.results.html_url, 
-                
-            };
+                profile: fetch.Response.results.html_url,
 
+            };
+        })
+    })
 // TODO: Create a function to write README file
-writeFileAsync("READMe.MD", genMarkdown(results, githubInformation), function(err) {
+writeFileAsync("READMe.MD", genMarkdown(results, githubInformation), function (err) {
     err ? console.error : console.log("READMe has been successfully created");
 });
 
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
