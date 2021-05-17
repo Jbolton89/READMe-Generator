430e1c4545a18ee2481593f2fc2d658694728371
Jbolts <77908382+Jbolton89@users.noreply.github.com>
2021-05-17 17:24:04 +1000

    more progress

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const fs = require("fs"); 
+const { ServerResponse } = require("http");
 const inquirer = require('inquirer');
 const fetch = require('node-fetch');
 const util = require('util');
@@ -58,21 +59,19 @@ inquirer
     .then(function(results){ 
         const url = `https://api.github.com/users/${results.username}`;
         fetch(url).then(function(result) {
-            const githubInformation = { 
-
-            }
-
-        })
-
-    }
+            const githubInformation = {
+                email: fetch.Response.results.email, 
+                image:fetch.Response.results.avatar_url, 
+                name: fetch.Response.results.name,
+                profile: fetch.Response.results.html_url, 
+                
+            };
 
 // TODO: Create a function to write README file
 writeFileAsync("READMe.MD", generate(results, githubInformation), function(err) {
-    if (error) { 
-        console.error;
-    }; 
-    console.log("READMe has been successfully created");
-}
+    err ? console.error : console.log("READMe has been successfully created");
+});
+
 
 // TODO: Create a function to initialize app
 function init() {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,8 +11,45 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(results) {
+  return `
+<h1 align="center">${results.title} 👋</h1>
+
+## Description
+${results.description}
+
+## Contents 
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Repository Link](#repository)
+- [GitHub Information](#github)
+
+##Installation 
+
+${results.installation}
+
+## Usage 
+
+${results.usage}
+
+##License 
+
+${results.licence}
+
+## Test 
+
+${results.test}
+
+## GitHub 
+
+![IMAGE](${githubInformation.image})
+- ${githubInformation.name}
+- [PROFILE](${githubInformation.profile})
+- <${githubInformation.email}>
 
 `;
 }
